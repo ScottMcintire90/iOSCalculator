f274d5a1853db99c820a8997ca2fa8dfb114361e
Scott McIntire <scottmcintire90@gmail.com>
2016-09-21 09:45:32 -0700

    add keyboard with π and √ buttons

--- iOSCalculator/ViewController.swift
+++ iOSCalculator/ViewController.swift
@@ -1,20 +1,45 @@
-//
-//  ViewController.swift
-//  iOSCalculator
-//
-//  Created by Cory Biggs on 9/16/16.
-//
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func touchDigit(sender: UIButton) {
-        
-        print("touchDigit")
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
     }
-
-
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(sender: UIButton) {
+        
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        displayValue = brain.result
+        
+    }
+    
 }
 
